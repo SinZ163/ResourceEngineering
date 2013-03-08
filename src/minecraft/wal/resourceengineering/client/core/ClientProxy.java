74996a9c22e39f6e64f57a887f305b0ee8c42bed
kg6jay <kg6jay@gmail.com>
2013-03-08 09:08:09 -0800

    Added gem block textures

--- src/minecraft/wal/resourceengineering/client/core/ClientProxy.java
+++ src/minecraft/wal/resourceengineering/client/core/ClientProxy.java
@@ -8,5 +8,6 @@ public class ClientProxy extends CommonProxy
 	public void registerRenderInformation()
 	{
 		MinecraftForgeClient.preloadTexture("/walgfx/Items.png");
+		MinecraftForgeClient.preloadTexture("/walgfx/Blocks.png");
 	}
 }
