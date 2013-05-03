d01ce474c151f0302166fce174b0900941e0ac94
kg6jay <kg6jay@gmail.com>
2013-05-03 10:16:43 -0700

    Tweaking Dirt Layer to be side specific

--- src/minecraft/wal/resourceengineering/common/core/items/tools/ItemToolDirtLayer.java
+++ src/minecraft/wal/resourceengineering/common/core/items/tools/ItemToolDirtLayer.java
@@ -3,6 +3,8 @@ package resourceengineering.common.core.items.tools;
 import java.util.ArrayList;
 import java.util.logging.Level;
 
+import cpw.mods.fml.common.FMLLog;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLiving;
@@ -15,6 +17,7 @@ import resourceengineering.common.ResourceEngineeringMain;
 
 public class ItemToolDirtLayer extends ItemTool
 {
+	private int targetBlockID = 3;
 	public static final Block[] blocksEffectiveAgainst = Block.blocksList;
 	public ItemToolDirtLayer(int ID, EnumToolMaterial m, int tex, String name)
 	{
@@ -55,12 +58,63 @@ public class ItemToolDirtLayer extends ItemTool
 			return false;
 		}
 	}
+	private void dropItems(World world,int i,int y,int j,int x,int z)
+	{
+		if (!world.isRemote && world.getGameRules().getGameRuleBooleanValue("doTileDrops"))
+	    {
+			ArrayList list = Block.blocksList[world.getBlockId(i,y,j)].getBlockDropped(world,i,y,j,world.getBlockMetadata(i,y+1,j),0);
+	    	if(list!=null)
+	    	{
+	    		for(int a=0;a<list.size();a++)
+	    		{
+	    			
+	    			ItemStack temp = (ItemStack)list.get(a);
+	    			EntityItem dropItem = new EntityItem(world,(double)(x),(double)(y),(double)(z),temp);
+	    			dropItem.delayBeforeCanPickup=0;
+	    			world.spawnEntityInWorld(dropItem);
+	    		}
+	    	}
+	    	else
+	    	{
+	    		ItemStack dropStack = new ItemStack(Block.blocksList[world.getBlockId(i,y+1,j)].idDropped(world.getBlockMetadata(i,y+1,j), world.rand, 0),1,world.getBlockMetadata(i,y+1,j));
+	    		EntityItem itemDrop = new EntityItem(world, (double)(x), (double)(y), (double)(z), dropStack);
+	            itemDrop.delayBeforeCanPickup = 0;
+	            world.spawnEntityInWorld(itemDrop);
+	        }
+	    }
+    }
 	@Override
-	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int blockFace, float par8, float par9, float par10)
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int yIn, int z, int blockFace, float par8, float par9, float par10)
     {
-		if(isBlockReplaceable(world.getBlockId(x,y,z)))
+		FMLLog.log(Level.INFO,"BlockFace: %s",blockFace);
+		int xStart,xEnd,yStart,yEnd,zStart,zEnd;
+		switch(blockFace)
 		{
-			y-=1;
+		case 0:
+		case 1://Top of Block
+			xStart = x-2;
+			xEnd = x+2;
+			yStart = yIn;
+			yEnd = yIn;
+			zStart = z-2;
+			zEnd = z+2;
+			break;
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		default:
+			xStart = x-2;
+			xEnd = x+2;
+			yStart = yIn;
+			yEnd = yIn;
+			zStart = z-2;
+			zEnd = z+2;
+			break;
+		}
+		if(isBlockReplaceable(world.getBlockId(x,yIn,z)))
+		{
+			yIn-=1;
 		}
 		if(player.inventory.hasItem(3))
 		{
@@ -68,48 +122,30 @@ public class ItemToolDirtLayer extends ItemTool
 			{
 				for(int j=(z-2);j<=(z+2);j++)
 				{
-					if(world.isAirBlock(i, y+1, j))
+					for(int y=yStart;y<=yEnd;y++)
 					{
-						if(player.inventory.consumeInventoryItem(3))
+						if(world.isAirBlock(i, y+1, j))
 						{
-							world.setBlock(i, y+1, j, 3);
+							if(player.inventory.consumeInventoryItem(3))
+							{
+								world.setBlock(i, y, j, 3);
+							}
+							else
+							{
+								return true;
+							}
 						}
-						else
+						if(isBlockReplaceable(world.getBlockId(i,y,j)))
 						{
-							return true;
-						}
-					}
-					if(isBlockReplaceable(world.getBlockId(i,y+1,j)))
-					{
-						if(player.inventory.consumeInventoryItem(3))
-						{
-							if (!world.isRemote && world.getGameRules().getGameRuleBooleanValue("doTileDrops"))
-					        {
-								ArrayList list = Block.blocksList[world.getBlockId(i,y+1,j)].getBlockDropped(world,i,y+1,j,world.getBlockMetadata(i,y+1,j),0);
-				            	if(list!=null)
-				            	{
-				            		for(int a=0;a<list.size();a++)
-				            		{
-				            			
-				            			ItemStack temp = (ItemStack)list.get(a);
-				            			EntityItem dropItem = new EntityItem(world,(double)(x),(double)(y),(double)(z),temp);
-				            			dropItem.delayBeforeCanPickup=0;
-				            			world.spawnEntityInWorld(dropItem);
-				            		}
-				            	}
-				            	else
-				            	{
-				            		ItemStack dropStack = new ItemStack(Block.blocksList[world.getBlockId(i,y+1,j)].idDropped(world.getBlockMetadata(i,y+1,j), world.rand, 0),1,world.getBlockMetadata(i,y+1,j));
-				            		EntityItem itemDrop = new EntityItem(world, (double)(x), (double)(y), (double)(z), dropStack);
-						            itemDrop.delayBeforeCanPickup = 0;
-						            world.spawnEntityInWorld(itemDrop);
-						        }
-					        }
-							world.setBlock(i, y+1, j, 3);
-						}
-						else
-						{
-							return true;
+							if(player.inventory.consumeInventoryItem(3))
+							{
+								dropItems(world,i,y,j,x,z);
+								world.setBlock(i, y, j, 3);
+							}
+							else
+							{
+								return true;
+							}
 						}
 					}
 				}
